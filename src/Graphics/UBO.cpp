fc922533ebd6b3c3b45808a27f7ac990a6c279f8
metinis <denis.muraska@gmail.com>
2024-10-18 23:15:33 +0100

    frustum culling test

---
+++ src/Graphics/UBO.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Denis Muraska on 12/10/2024.
+//
+
+#include "UBO.h"
---
+++ src/Graphics/UBO.h
@@ -0,0 +1,16 @@
+//
+// Created by Denis Muraska on 12/10/2024.
+//
+
+#ifndef UBO_H
+#define UBO_H
+
+
+
+class UBO {
+
+};
+
+
+
+#endif //UBO_H
